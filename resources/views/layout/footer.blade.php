12face269e377cb7c0bf217a671e0370e809fc62
Rohit Gupta <rohittgupta379@email@gmail.com>
2026-07-11 19:17:35 +0530

    first commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,73 @@
+ <!--**********************************
+            Footer start
+        ***********************************-->
+ <div class="footer">
+
+ <div class="copyright">
+ <p>Copyright © Designed &amp; Developed by <a href="https://dexignzone.com/" target="_blank">DexignZone</a> 2023
+ </p>
+ </div>
+ </div>
+ <!--**********************************
+            Footer end
+        ***********************************-->
+
+ <!--**********************************
+           Support ticket button start
+        ***********************************-->
+
+ <!--**********************************
+           Support ticket button end
+        ***********************************-->
+
+
+ </div>
+ <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+
+
+
+
+
+
+
+ <!--**********************************
+        Scripts
+    ***********************************-->
+ <!-- Required vendors -->
+ <script src="vendor/global/global.min.js"></script>
+ <script src="vendor/chart.js/Chart.bundle.min.js"></script>
+ <script src="vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+
+ <!-- Chart piety plugin files -->
+ <script src="vendor/peity/jquery.peity.min.js"></script>
+
+ <!-- Apex Chart -->
+ <script src="vendor/apexchart/apexchart.js"></script>
+
+ <script src="vendor/bootstrap-datetimepicker/js/moment.js"></script>
+ <script src="vendor/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+
+ <!-- Dashboard 1 -->
+ <script src="js/dashboard/dashboard-1.js"></script>
+ <script src="js/custom.min.js"></script>
+ <script src="js/deznav-init.js"></script>
+
+
+ <script>
+     $(function() {
+         $('#datetimepicker').datetimepicker({
+             inline: true,
+         });
+     });
+
+     $(document).ready(function() {
+         $(".booking-calender .fa.fa-clock-o").removeClass(this);
+         $(".booking-calender .fa.fa-clock-o").addClass('fa-clock');
+     });
+ </script>
+ </body>
+
+ </html>
