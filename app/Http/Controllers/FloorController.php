ae133cadfa89d990bd3e9de5ad0e53de8474df55
helodeepakji <helodeepakji@gmail.com>
2026-07-12 11:08:24 +0530

    crud floor

--- app/Http/Controllers/FloorController.php
+++ app/Http/Controllers/FloorController.php
@@ -12,7 +12,7 @@ class FloorController extends Controller
     {
         $floors = Floor::all();
         $rooms = Room::all();
-        return view('floor',['floors' => $floors,'rooms' => $rooms]);
+        return view('floor', ['floors' => $floors, 'rooms' => $rooms]);
     }
 
     public function addFloor(Request $request)
@@ -34,10 +34,38 @@ class FloorController extends Controller
         return back()->with('success', 'Floor added successfully.');
     }
 
+    public function deleteFloor($id)
+    {
+        $floor = Floor::find($id);
+
+        if (!$floor) {
+            return back()->with('error', 'Floor not found.');
+        }
+
+        $floor->delete();
+
+        return back()->with('success', 'Floor deleted successfully.');
+    }
+
+
+    public function updateFloor(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'id' => ['required', 'exists:floors,id'],
+        ]);
+
+        $floor = Floor::find($request->id);
+        $floor->name = $request->name;
+        $floor->save();
+
+        return back()->with('success', 'Floor updated successfully.');
+    }
+
     // Add room
 
 
-     public function viewRooms()
+    public function viewRooms()
     {
         return view('rooms');
     }
@@ -59,6 +87,4 @@ class FloorController extends Controller
 
         return back()->with('success', 'Room type added successfully.');
     }
-
-   
 }
--- resources/views/floor.blade.php
+++ resources/views/floor.blade.php
@@ -2,53 +2,55 @@
 
 
 
-    <!-- row -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="d-flex mb-4 justify-content-between align-items-center flex-wrap">
-                    <div class="card-tabs mt-3 mt-sm-0 mb-xxl-0 mb-4">
-                        <ul class="nav nav-tabs" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" data-bs-toggle="tab" href="#floors" role="tab">All Floors ({{count($floors ?? [])}})</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="tab" href="#rooms" role="tab">All Room (({{count($rooms ?? [])}}))</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="table-search">
-                        <div class="input-group search-area mb-xxl-0 mb-4">
-                            <input type="text" class="form-control" placeholder="Search here">
-                            <span class="input-group-text"><a href="javascript:void(0)"><i
-                                        class="flaticon-381-search-2"></i></a></span>
-                        </div>
-                    </div>
-                    <a href="javascript:void(0);" class="btn btn-primary mb-xxl-0 mb-4 radius-btn"
-                        data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="far fa-file-word me-2"></i>Add
-                        Floors</a>
+<!-- row -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="d-flex mb-4 justify-content-between align-items-center flex-wrap">
+                <div class="card-tabs mt-3 mt-sm-0 mb-xxl-0 mb-4">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-bs-toggle="tab" href="#floors" role="tab">All Floors
+                                ({{ count($floors ?? []) }})</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#rooms" role="tab">All Room
+                                ({{ count($rooms ?? []) }})</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="tab-content">
-                    <div class="tab-pane active show" id="floors">
-                        <div class="table-responsive">
-                            <table
-                                class="table card-table check-data  display mb-4 dataTablesCard booking-table room-list-tbl table-responsive-lg "
-                                id="guestTable-floors">
-                                <thead>
-                                    <tr>
-                                        <th class="bg-none">
-                                            <div class="form-check style-1">
-                                                <input class="form-check-input checkAll" type="checkbox" value="">
-                                            </div>
-                                        </th>
-                                        <th>Sno</th>
-                                        <th>Name</th>
-                                        <th>Created At</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($floors as $index => $floor)
+                <div class="table-search">
+                    <div class="input-group search-area mb-xxl-0 mb-4">
+                        <input type="text" class="form-control" placeholder="Search here">
+                        <span class="input-group-text"><a href="javascript:void(0)"><i
+                                    class="flaticon-381-search-2"></i></a></span>
+                    </div>
+                </div>
+                <a href="javascript:void(0);" class="btn btn-primary mb-xxl-0 mb-4 radius-btn" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"><i class="far fa-file-word me-2"></i>Add
+                    Floors</a>
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane active show" id="floors">
+                    <div class="table-responsive">
+                        <table
+                            class="table card-table check-data  display mb-4 dataTablesCard booking-table room-list-tbl table-responsive-lg "
+                            id="guestTable-floors">
+                            <thead>
+                                <tr>
+                                    <th class="bg-none">
+                                        <div class="form-check style-1">
+                                            <input class="form-check-input checkAll" type="checkbox" value="">
+                                        </div>
+                                    </th>
+                                    <th>Sno</th>
+                                    <th>Name</th>
+                                    <th>Created At</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($floors as $index => $floor)
                                     <tr>
                                         <td>
                                             <div class="form-check style-1">
@@ -57,17 +59,17 @@
                                         </td>
                                         <td>
                                             <div>
-                                                <span class="fs-16">{{++$index}}</span>
+                                                <span class="fs-16">{{ ++$index }}</span>
                                             </div>
                                         </td>
                                         <td>
                                             <div>
-                                                <span class="fs-16">{{$floor->name}}</span>
+                                                <span class="fs-16">{{ $floor->name }}</span>
                                             </div>
                                         </td>
                                         <td>
                                             <div>
-                                                <span class="fs-16">{{$floor->created_at}}</span>
+                                                <span class="fs-16">{{ $floor->created_at }}</span>
                                             </div>
                                         </td>
                                         <td>
@@ -91,39 +93,42 @@
                                                     </svg>
                                                 </a>
                                                 <div class="dropdown-menu">
-                                                    <a class="dropdown-item" onclick="editFloor({{ $floor->id }})">Edit</a>
-                                                    <a class="dropdown-item" onclick="deleteFloor({{ $floor->id }})">Delete</a>
+                                                    <a class="dropdown-item" data-bs-toggle="modal"
+                                                        data-bs-target="#editModal"
+                                                        onclick="editFloor({{ $floor->id }},'{{ $floor->name }}')">Edit</a>
+                                                    <a class="dropdown-item"
+                                                        onclick="deleteFloor({{ $floor->id }})">Delete</a>
                                                 </div>
                                             </div>
                                         </td>
                                     </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                <div class="tab-content">
-                    <div class="tab-pane" id="rooms">
-                        <div class="table-responsive">
-                            <table
-                                class="table card-table check-data  display mb-4 dataTablesCard booking-table room-list-tbl table-responsive-lg "
-                                id="guestTable-floors">
-                                <thead>
-                                    <tr>
-                                        <th class="bg-none">
-                                            <div class="form-check style-1">
-                                                <input class="form-check-input checkAll" type="checkbox" value="">
-                                            </div>
-                                        </th>
-                                        <th>Sno</th>
-                                        <th>Name</th>
-                                        <th>Created At</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($rooms as $index => $room)
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane" id="rooms">
+                    <div class="table-responsive">
+                        <table
+                            class="table card-table check-data  display mb-4 dataTablesCard booking-table room-list-tbl table-responsive-lg "
+                            id="guestTable-floors">
+                            <thead>
+                                <tr>
+                                    <th class="bg-none">
+                                        <div class="form-check style-1">
+                                            <input class="form-check-input checkAll" type="checkbox" value="">
+                                        </div>
+                                    </th>
+                                    <th>Sno</th>
+                                    <th>Name</th>
+                                    <th>Created At</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($rooms as $index => $room)
                                     <tr>
                                         <td>
                                             <div class="form-check style-1">
@@ -132,17 +137,17 @@
                                         </td>
                                         <td>
                                             <div>
-                                                <span class="fs-16">{{++$index}}</span>
+                                                <span class="fs-16">{{ ++$index }}</span>
                                             </div>
                                         </td>
                                         <td>
                                             <div>
-                                                <span class="fs-16">{{$room->name}}</span>
+                                                <span class="fs-16">{{ $room->name }}</span>
                                             </div>
                                         </td>
                                         <td>
                                             <div>
-                                                <span class="fs-16">{{$room->created_at}}</span>
+                                                <span class="fs-16">{{ $room->created_at }}</span>
                                             </div>
                                         </td>
                                         <td>
@@ -166,21 +171,23 @@
                                                     </svg>
                                                 </a>
                                                 <div class="dropdown-menu">
-                                                    <a class="dropdown-item" onclick="editFloor({{ $room->id }})">Edit</a>
-                                                    <a class="dropdown-item" onclick="deleteFloor({{ $room->id }})">Delete</a>
+                                                    <a class="dropdown-item"
+                                                        onclick="editRoom({{ $room->id }},'{{ $room->name }}')">Edit</a>
+                                                    <a class="dropdown-item"
+                                                        onclick="deleteRoom({{ $room->id }})">Delete</a>
                                                 </div>
                                             </div>
                                         </td>
                                     </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
 <!-- Modal -->
@@ -199,7 +206,8 @@
                         <div class="col-xl-12">
                             <div class="mb-3">
                                 <label for="" class="form-label">Floor Name</label>
-                                <input type="text" class="form-control" name="name" placeholder="First Floor, Ground Floor,etc">
+                                <input type="text" class="form-control" name="name"
+                                    placeholder="First Floor, Ground Floor,etc">
                             </div>
                         </div>
                     </div>
@@ -213,6 +221,47 @@
     </div>
 </div>
 
-
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Update Floor</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="/update-floor" method="post">
+                    @csrf
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="mb-3">
+                                <label class="form-label">Floor Name</label>
+                                <input type="text" class="form-control" id="floor_name" name="name"
+                                    placeholder="First Floor, Ground Floor,etc">
+                                <input type="hidden" name="id" id="floor_id">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Update changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 @include('layout.footer');
+<script>
+    function editFloor(id, name) {
+        $('#floor_id').val(id);
+        $('#floor_name').val(name);
+    }
+
+    function deleteFloor(id) {
+        if (confirm("Are you sure you want to delete this floor?")) {
+            window.location.href = '/delete-floor/' + id;
+        }
+    }
+</script>
