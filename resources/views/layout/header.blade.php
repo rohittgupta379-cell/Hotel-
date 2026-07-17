ac1f5c51d5c1fecef3999778d89c7aab32675fc9
helodeepakji <helodeepakji@gmail.com>
2026-07-17 11:02:10 +0530

    add bill

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -843,78 +843,6 @@
                                             class="ti-arrow-end"></i></a>
                                 </div>
                             </li>
-                            <li class="nav-item dropdown notification_dropdown">
-                                <a class="nav-link ai-icon" href="javascript:void(0);" data-bs-toggle="dropdown">
-                                    <svg width="24" height="22" viewBox="0 0 24 22" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd"
-                                            d="M2.15608 11.6592C0.937571 10.4299 0.253296 8.76839 0.253296 7.03607C0.253296 5.29415 0.944772 3.62306 2.17648 2.39134C3.4082 1.15963 5.0793 0.46814 6.82122 0.46814C8.56315 0.46814 10.2342 1.15963 11.466 2.39134L11.9149 2.84033L12.3639 2.39134C13.5956 1.15963 15.2655 0.46814 17.0075 0.46814C18.7506 0.46814 20.4205 1.15963 21.6522 2.39134C22.8839 3.62306 23.5766 5.29415 23.5766 7.03607C23.5766 8.76839 22.8923 10.4299 21.6726 11.6592L12.7625 21.0939C12.5428 21.3268 12.2355 21.4589 11.9149 21.4589C11.5944 21.4589 11.2871 21.3268 11.0674 21.0939L2.15608 11.6592ZM11.9149 18.5945L19.9799 10.0553L20.0039 10.0313C20.7974 9.23659 21.244 8.15974 21.244 7.03607C21.244 5.9124 20.7974 4.83556 20.0039 4.04083C19.2092 3.2461 18.1311 2.79951 17.0075 2.79951C15.885 2.79951 14.807 3.2461 14.0122 4.04083L12.7397 5.31456C12.2835 5.76955 11.5452 5.76955 11.0902 5.31456L9.81646 4.04083C9.02293 3.2461 7.94489 2.79951 6.82122 2.79951C5.69756 2.79951 4.62071 3.2461 3.82598 4.04083C3.03125 4.83556 2.58586 5.9124 2.58586 7.03607C2.58586 8.15974 3.03125 9.23659 3.82598 10.0313C3.83438 10.0397 3.84158 10.0469 3.84879 10.0553L11.9149 18.5945Z"
-                                            fill="#1362FC" />
-                                    </svg>
-                                    <div class="pulse-css"></div>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <div id="DZ_W_TimeLine02" class="widget-timeline dz-scroll style-1 p-3 height370">
-                                        <ul class="timeline">
-                                            <li>
-                                                <div class="timeline-badge primary"></div>
-                                                <a class="timeline-panel text-muted" href="javascript:void(0);">
-                                                    <span>10 minutes ago</span>
-                                                    <h6 class="mb-0">Youtube, a video-sharing website, goes live
-                                                        <strong class="text-primary">$500</strong>.
-                                                    </h6>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <div class="timeline-badge info">
-                                                </div>
-                                                <a class="timeline-panel text-muted" href="javascript:void(0);">
-                                                    <span>20 minutes ago</span>
-                                                    <h6 class="mb-0">New order placed <strong
-                                                            class="text-info">#XF-2356.</strong></h6>
-                                                    <p class="mb-0">Quisque a consequat ante Sit amet magna at
-                                                        volutapt...</p>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <div class="timeline-badge danger">
-                                                </div>
-                                                <a class="timeline-panel text-muted" href="javascript:void(0);">
-                                                    <span>30 minutes ago</span>
-                                                    <h6 class="mb-0">john just buy your product <strong
-                                                            class="text-warning">Sell $250</strong></h6>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <div class="timeline-badge success">
-                                                </div>
-                                                <a class="timeline-panel text-muted" href="javascript:void(0);">
-                                                    <span>15 minutes ago</span>
-                                                    <h6 class="mb-0">StumbleUpon is acquired by eBay. </h6>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <div class="timeline-badge warning">
-                                                </div>
-                                                <a class="timeline-panel text-muted" href="javascript:void(0);">
-                                                    <span>20 minutes ago</span>
-                                                    <h6 class="mb-0">Mashable, a news website and blog, goes live.
-                                                    </h6>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <div class="timeline-badge dark">
-                                                </div>
-                                                <a class="timeline-panel text-muted" href="javascript:void(0);">
-                                                    <span>20 minutes ago</span>
-                                                    <h6 class="mb-0">Mashable, a news website and blog, goes live.
-                                                    </h6>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
                             <li class="nav-item dropdown header-profile">
                                 <a class="nav-link" href="javascript:void(0);" role="button"
                                     data-bs-toggle="dropdown">
@@ -925,7 +853,7 @@
                                     </div>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end">
-                                    <a href="app-profile.html" class="dropdown-item ai-icon">
+                                    <a href="/profile" class="dropdown-item ai-icon">
                                         <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary"
                                             width="18" height="18" viewBox="0 0 24 24" fill="none"
                                             stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -935,19 +863,7 @@
                                         </svg>
                                         <span class="ms-2">Profile </span>
                                     </a>
-                                    <a href="email-inbox.html" class="dropdown-item ai-icon">
-                                        <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" class="text-success"
-                                            width="18" height="18" viewBox="0 0 24 24" fill="none"
-                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path
-                                                d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
-                                            </path>
-                                            <polyline points="22,6 12,13 2,6"></polyline>
-                                        </svg>
-                                        <span class="ms-2">Inbox </span>
-                                    </a>
-                                    <a href="page-login.html" class="dropdown-item ai-icon">
+                                    <a href="/logout" class="dropdown-item ai-icon">
                                         <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger"
                                             width="18" height="18" viewBox="0 0 24 24" fill="none"
                                             stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -1034,65 +950,29 @@
 
                         </ul>
                     </li>
+                    
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-086-star"></i>
+                            <span class="nav-text">Bill Managment</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="#">Hotel Bills</a></li>
+                            <li><a href="#">Food Bills</a></li>
+                            <li><a href="#">Expense</a></li>
+                        </ul>
+                    </li>
 
-
-
-                    {{--  --}}
-                    <li>
-                        <a class="has-arrow ai-icon" href="{{ url('complaints') }}" aria-expanded="false">
-                            <i class="fa fa-exclamation-circle"></i>
+                    <li><a href="{{ url('complaints') }}" class="ai-icon" aria-expanded="false">
+                            <i class="flaticon-013-checkmark"></i>
                             <span class="nav-text">Complaint</span>
                         </a>
                     </li>
-                    <li><a href="widget-basic.html" class="ai-icon" aria-expanded="false">
-                            <i class="flaticon-013-checkmark"></i>
-                            <span class="nav-text">Widget</span>
-                        </a>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="flaticon-072-printer"></i>
-                            <span class="nav-text">Forms</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="form-element.html">Form Elements</a></li>
-                            <li><a href="form-wizard.html">Wizard</a></li>
-                            <li><a href="form-ckeditor.html">CkEditor</a></li>
-                            <li><a href="form-pickers.html">Pickers</a></li>
-                            <li><a href="form-validation.html">Form Validate</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="flaticon-043-menu"></i>
-                            <span class="nav-text">Table</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="table-bootstrap-basic.html">Bootstrap</a></li>
-                            <li><a href="table-datatable-basic.html">Datatable</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="flaticon-022-copy"></i>
-                            <span class="nav-text">Pages</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="page-login.html">Login</a></li>
-                            <li><a href="page-register.html">Register</a></li>
-                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Error</a>
-                                <ul aria-expanded="false">
-                                    <li><a href="page-error-400.html">Error 400</a></li>
-                                    <li><a href="page-error-403.html">Error 403</a></li>
-                                    <li><a href="page-error-404.html">Error 404</a></li>
-                                    <li><a href="page-error-500.html">Error 500</a></li>
-                                    <li><a href="page-error-503.html">Error 503</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="page-lock-screen.html">Lock Screen</a></li>
-                            <li><a href="empty-page.html">Empty Page</a></li>
-                        </ul>
-                    </li>
+
+                    
+
                 </ul>
                 <div class="copyright">
-                    <p><strong>Innap Hotel Admin</strong> © 2023 All Rights Reserved</p>
+                    <p><strong>Innap Hotel Admin</strong> © {{date('Y')}} All Rights Reserved</p>
                     <p class="fs-12">Made with <span class="heart"></span> by DexignZone</p>
                 </div>
             </div>
